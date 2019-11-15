80f084b36ed28fec8ec02501f47a91d7362cb898
fmi <fmi@fmi.uni-plovdiv.bg>
2019-11-15 11:04:34 +0200

    Changed to wrong name

--- ConsoleApp1/Tests.cs
+++ ConsoleApp1/Tests.cs
@@ -22,7 +22,7 @@ namespace FMIExamples
             driver.Navigate().GoToUrl("https://www.mail.bg/");
             driver.Manage().Window.Maximize();
             
-            IWebElement username = driver.FindElement(By.Id("username")); 
+            IWebElement username = driver.FidElement(By.Id("username")); 
             IWebElement password = driver.FindElement(By.Id("password"));
             IWebElement loginBtn = driver.FindElement(By.Id("loginBut"));
             IWebElement lostPasswordLink = driver.FindElement(By.Id("lostPasswordLink"));
